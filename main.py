730ed53ad50ab2382a475fe2c7d6d15a93fa74d1
Sean aye <hello@seanaye.ca>
2021-04-14 12:41:42 -0400

    plot line of best fit

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from os import listdir
 from os.path import isfile, join
 from datetime import datetime, timezone
 import matplotlib.pyplot as plt
+import numpy as np
 
 def ms_epoch_to_month(row):
     ms = row['date']
     sec = ms // 1000
     dt = datetime.fromtimestamp(sec, timezone.utc)
-    return dt.strftime("%m/%Y")
+    return dt.strftime("%Y")
 
 def ship_to_ice(row):
-    return row['ship'] / row['ice']
+    return row['ship'] / row['ice-coverage']
 
 def ice_coverage_percent(row):
     ratio = row['ice'] / sum([row['ship'], row['ice'], row['water'], row['unknown']])
@@ -29,6 +30,7 @@ def main():
 
     df = pd.concat(to_merge, ignore_index=True).drop_duplicates(subset=['date'])
     df = df[(df.date > 0) & (df.ship > 0) & (df.ice > 0)]
+    df = df[(df.ship < 100)]
     df.drop('.geo', axis=1, inplace=True)
     df.drop('system:index', axis=1, inplace=True)
     # add human readable times
@@ -36,12 +38,22 @@ def main():
 
     df = df.groupby('time').mean()
 
-    df['ship-to-ice'] = df.apply(ship_to_ice, axis=1)
-    df['ice_coverage'] = df.apply(ice_coverage_percent, axis=1)
+    df['ice-coverage'] = df.apply(ice_coverage_percent, axis=1)
+    df['ship-to-ice-coverage'] = df.apply(ship_to_ice, axis=1)
     df = df.sort_values(by=['date']).reset_index()
 
+    y_val = 'ship'
+    x = df['date']
+    m, b = np.polyfit(x, df[y_val], 1)
+
     print(df)
-    df.plot(x='time', y='ship-to-ice')
+    df.plot(x='date', y=[y_val], ylabel="avg #px per day")
+    plt.plot(x, m*x+b)
+    plt.title('Ship Traffic per Day per Year')
+    #plt.plot('time', 'ice-coverage', data=df)
+    #plt.plot( 'time', 'ice-coverage', data=df, color='skyblue', linewidth=4)
+    #plt.plot( 'time', 'ship', data=df, marker='', color='olive', linewidth=2)
+    plt.grid()
     plt.show()
 
 if __name__ == '__main__':
